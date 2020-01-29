22cb521c35c22a1921e84146ac4a40df58cadb00
MARIO LOPEZ <m-lopez22@live.com>
2020-01-28 18:48:50 -0800

    updated server.js

---
+++ app/data/friend.js
@@ -0,0 +1,100 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [{
+      name: 'Hank Hill',
+      profilePic: 'https://i.kym-cdn.com/photos/images/facebook/000/116/460/tumblr_ljoch8Y0bk1qce1j4o1_250.jpg',
+      scores: [
+        "2",
+        "5",
+        "5",
+        "3",
+        "5",
+        "2",
+        "1",
+        "1",
+        "5",
+        "1"
+      ]
+    },
+    {
+      name: 'Peter Griffin',
+      profilePic: 'https://pbs.twimg.com/profile_images/477441312874979329/1_HI3YJr.jpeg',
+      scores: [
+        "5",
+        "5",
+        "1",
+        "4",
+        "1",
+        "1",
+        "4",
+        "5",
+        "1",
+        "5"
+      ]
+    },
+    {
+      name: 'Homer Simpson',
+      profilePic: 'http://profilepicturesdp.com/wp-content/uploads/2018/06/homer-simpson-profile-picture-5.jpg',
+      scores: [
+        "4",
+        "5",
+        "1",
+        "4",
+        "1",
+        "3",
+        "5",
+        "4",
+        "3",
+        "5"
+      ]
+    },
+    {
+      name: 'Bob Belcher',
+      profilePic: 'https://hollywoodhatesme.files.wordpress.com/2017/10/bob.jpg',
+      scores: [
+        "2",
+        "2",
+        "4",
+        "3",
+        "5",
+        "4",
+        "1",
+        "2",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: 'Tina Belcher',
+      profilePic: 'https://hollywoodhatesme.files.wordpress.com/2016/09/tina-belcher-smiling.jpg?w=495',
+      scores: [
+        "1",
+        "1",
+        "4",
+        "2",
+        "1",
+        "5",
+        "5",
+        "3",
+        "2",
+        "4"
+      ]
+    },
+    {
+      name: 'Rick Sanchez',
+      profilePic: 'https://orig00.deviantart.net/6485/f/2014/084/4/f/rick_sanchez_from_rick_and_morty_by_zhenya9876-d7bk3kv.png',
+      scores: [
+        "5",
+        "4",
+        "2",
+        "5",
+        "1",
+        "1",
+        "5",
+        "5",
+        "1",
+        "5"
+      ]
+    }
+  ]
+    module.exports = friendList;
